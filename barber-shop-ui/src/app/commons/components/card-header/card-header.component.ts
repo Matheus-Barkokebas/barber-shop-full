bab0df53ddaa37b7fb6c67e8ef790a596d8d89f4
Matheus <matheusbarkcaribe@gmail.com>
2025-03-17 13:49:46 -0300

    Criacao do header e menu de navegacao

--- barber-shop-ui/src/app/commons/components/card-header/card-header.component.ts
+++ barber-shop-ui/src/app/commons/components/card-header/card-header.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-card-header',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule],
   templateUrl: './card-header.component.html',
   styleUrl: './card-header.component.scss'
 })
